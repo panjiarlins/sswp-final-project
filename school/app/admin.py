149276b890fcd142b2df3665ce1049dfe6b351af
Panji Arlin Saputa <panjiarlinsaputra27@gmail.com>
2020-12-25 16:54:21 +0700

    Re-migrating and solving the typo in admin.py

--- school/app/admin.py
+++ school/app/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Subject, Employement, Employee, Student, Extracurricular, Class
+from .models import Subject, Employment, Employee, Student, Extracurricular, Class
 admin.site.register(Subject)
-admin.site.register(Employement)
+admin.site.register(Employment)
 admin.site.register(Employee)
 admin.site.register(Student)
 admin.site.register(Class)
