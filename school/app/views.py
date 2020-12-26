88be02dbdf7140f7a51929324596a9b22e11ec42
Panji Arlin Saputa <panjiarlinsaputra27@gmail.com>
2020-12-26 17:18:29 +0700

    Adding class, employment, and extracurricular into Index menu

--- school/app/views.py
+++ school/app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.forms.models import model_to_dict
 from django.contrib.auth.decorators import login_required
-
+from .models import Subject, Employee, Student, Class, Employment, Extracurricular
 # Create your views here.
 import requests, ast
 def covid_info(request):
@@ -16,13 +16,15 @@ def covid_info(request):
         context = ast.literal_eval(str(data))
     return render(request, 'appTemp/covid_info.html', context)
 
-from .models import Subject, Employee, Student
 def index(request):
     # we check the session with key ‘num_visits’, if doesn’t exist we set to 1
     num_visits = request.session.get('num_visits', 1)
     request.session['num_visits'] = num_visits + 1  # add everytime we reload
     context = {
         'num_subject' : Subject.objects.all().count(),
+        'num_class' : Class.objects.all().count(),
+        'num_employment' : Employment.objects.all().count(),
+        'num_extracurricular' : Extracurricular.objects.all().count(),
         'num_employee' : Employee.objects.all().count(),
         'num_student' : Student.objects.all().count(),
         'total_visit': num_visits,
